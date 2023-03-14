60e3a3558cc3a94da09d9756aa6f013e02a5be80
jkb114 <jkb114@gmail.com>
2023-03-14 09:18:39 +0000

    Moving stuff around

---
+++ UI/HackFrontend/Pages/Cars.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using HackFrontend.Objects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace HackFrontend.Pages
+{
+    public class CarsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public IEnumerable<Cars> CarsIE { get; set;}
+        public CarsModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+
+
+        }
+
+        public Task OnGetAsync()
+        {
+            var aPiHelper = new ApiHelper(_configuration);
+            var url = aPiHelper.GetUri("AuthenticationService");
+            var response = aPiHelper.SendApIrequest(url, Method.Post, User);
+            CarsIE =  JsonConvert.DeserializeObject <IEnumerable<Cars>>(response);
+
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UI/HackFrontend/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HackFrontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+        public string Username { get; set; }
+        public bool IsLoggedIn { get; set; }    
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+
+
+        }
+
+        public Task OnGetAsync()
+        {
+
+
+            IsLoggedIn = Convert.ToBoolean(HttpContext.Session.GetString("Logged_in"));
+            if (IsLoggedIn)
+            {
+                Username = HttpContext.Session.GetString("Username");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
